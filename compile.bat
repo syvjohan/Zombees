2d9b57741408741a834b00b87d4df49ff2a3724c
Peter Mårtensson <veritas.is.me@gmail.com>
2013-04-01 04:29:51 +0200

    Changed makejar to just make a jar file in the bin folder. Changed compile to make clean builds every time.
    This way we can run and debug using the class files and the debug flags, and make jar files for distribution.
    
    Signed-off-by: Peter Mårtensson <veritas.is.me@gmail.com>

--- compile.bat
+++ compile.bat
@@ -1,4 +1,6 @@
 
+:: Clean build
+FOR /F "tokens=*" %%G IN ('DIR /B /S *.class') DO DEL /S /Q %%G
 
 for /r %%f in (*.java) do (
  javac -sourcepath source -cp lwjgl\jar\lwjgl.jar;lwjgl\jar\lwjgl_util.jar %%f -d bin\class
--- makejar.bat
+++ makejar.bat
@@ -1,8 +1,10 @@
 
-FOR /F "tokens=*" %%G IN ('DIR /B /S *.class') DO DEL /S /Q %%G
-call compile.bat
+::It's probably better if we just make rars for distributing the software.
+
+::FOR /F "tokens=*" %%G IN ('DIR /B /S *.class') DO DEL /S /Q %%G
+::call compile.bat
 
 DEL bin\jar\Zombees.jar
 jar -0vcmf manifest.txt bin\jar\Zombees.jar -C bin\class\ .
 
-call runjar.bat
+::call runjar.bat
